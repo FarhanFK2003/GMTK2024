d91813b3cf56a7b81cfceaeb2136d54ead4ca744
FarhanFK2003 <farhan.khanfk2003@gmail.com>
2024-08-17 20:45:45 +0500

    Player Animations Done

--- GMTK2024/Assets/Scripts/PlayerController.cs
+++ GMTK2024/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Speed at which the player will move
-    public float speed = 5.0f;
+    public float speed = 5.0f;      // Speed at which the player will move
+    public Transform playerModel;   // Reference to the player model (to rotate)
+    public Animator animator;       // Reference to the Animator component
 
     // Update is called once per frame
     void Update()
@@ -17,5 +18,42 @@ public class PlayerController : MonoBehaviour
 
         // Move the player
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+        // Rotate the player model to face the movement direction
+        if (movement != Vector3.zero)
+        {
+            playerModel.rotation = Quaternion.LookRotation(movement);
+            animator.SetBool("isRunning", true);  // Set isRunning to true when the player is moving
+            animator.ResetTrigger("isIdle");      // Reset the idle trigger
+        }
+        else
+        {
+            animator.SetBool("isRunning", false); // Set isRunning to false when the player is not moving
+            animator.SetTrigger("isIdle");        // Set the idle trigger
+        }
+
+        // Check for roll action
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("isRoll");
+        }
+
+        // Check for kick action
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animator.SetTrigger("isKicking");
+        }
+
+        // Check for interact action
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("isInteract");
+        }
+
+        // Check for punch action
+        if (Input.GetMouseButtonDown(0)) // Left Mouse Button
+        {
+            animator.SetTrigger("isPunch");
+        }
     }
 }
