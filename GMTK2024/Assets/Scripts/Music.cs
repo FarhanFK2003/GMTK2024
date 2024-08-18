a5ce341aa4dae61596ac9859479942d6cbc126bf
FarhanFK2003 <farhan.khanfk2003@gmail.com>
2024-08-19 02:17:29 +0500

    Main Menu Functionality Done

---
+++ GMTK2024/Assets/Scripts/Music.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    static Music instance;
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
